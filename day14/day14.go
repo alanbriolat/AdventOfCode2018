ef1fee33d76fbad4535f76cc93f32d9a5d9cb866
Alan Briolat <alan.briolat@gmail.com>
2018-12-14 21:52:17 +0000

    Day 14 part2 (naive, slow)

--- day14/day14.go
+++ day14/day14.go
@@ -1,6 +1,7 @@
 package day14
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/alanbriolat/AdventOfCode2018/util"
 	"log"
@@ -9,18 +10,18 @@ import (
 func part1impl(logger *log.Logger, previous int, slice int) []byte {
 	t := util.NewTimer(logger, "")
 	defer t.LogCheckpoint("end")
-	
+
 	// How many recipes we need in total = sample length + number of preceding recipes
 	recipeCount := slice + previous
 	// Pre-allocate upper-bound of how many recipes there can be - account for potential overshoot
 	// of 1 if the last round is a double-digit score
-	recipes := make([]byte, 0, recipeCount + 1)
+	recipes := make([]byte, 0, recipeCount+1)
 	recipes = append(recipes, 3, 7)
 	elves := [2]int{0, 1}
-	
+
 	// Run the first round differently, because it's special
 	recipes = append(recipes, 1, 0)
-	
+
 	// Run rounds until we have enough recipes
 	for len(recipes) < recipeCount {
 		sum := byte(0)
@@ -30,7 +31,47 @@ func part1impl(logger *log.Logger, previous int, slice int) []byte {
 		}
 		// Turn the score total into more recipes
 		for _, x := range []byte(fmt.Sprint(sum)) {
-			recipes = append(recipes, x - '0') // Convert ASCII digit to integer
+			recipes = append(recipes, x-'0') // Convert ASCII digit to integer
+		}
+		// Move the elves
+		for i := range elves {
+			elves[i] = (elves[i] + 1 + int(recipes[elves[i]])) % len(recipes)
+		}
+	}
+
+	return recipes[previous : previous+slice]
+}
+
+func part2impl(logger *log.Logger, match []byte) int {
+	t := util.NewTimer(logger, "")
+	defer t.LogCheckpoint("end")
+
+	recipes := make([]byte, 0)
+	recipes = append(recipes, 3, 7)
+	elves := [2]int{0, 1}
+
+	matchStart := 0
+	matched := false
+
+	// Run the first round differently, because it's special
+	recipes = append(recipes, 1, 0)
+
+	// Run rounds until we find the match
+	for !matched {
+		sum := byte(0)
+		// Find the recipe score total
+		for _, i := range elves {
+			sum += recipes[i]
+		}
+		// Turn the score total into more recipes
+		for _, x := range []byte(fmt.Sprint(sum)) {
+			recipes = append(recipes, x-'0') // Convert ASCII digit to integer
+			if !matched && matchStart+len(match) <= len(recipes) {
+				matched = bytes.Equal(match, recipes[matchStart:matchStart+len(match)])
+				if !matched {
+					matchStart++
+				}
+			}
 		}
 		// Move the elves
 		for i := range elves {
@@ -38,7 +79,7 @@ func part1impl(logger *log.Logger, previous int, slice int) []byte {
 		}
 	}
 	
-	return recipes[previous:previous+slice]
+	return matchStart
 }
 
 func part1(logger *log.Logger, previous int, slice int) string {
@@ -49,8 +90,21 @@ func part1(logger *log.Logger, previous int, slice int) string {
 	return string(result)
 }
 
+func part2(logger *log.Logger, input int) string {
+	match := []byte(fmt.Sprint(input))
+	for i := range match {
+		match[i] -= '0'
+	}
+	result := part2impl(logger, match)
+	return fmt.Sprint(result)
+}
+
 func init() {
 	util.RegisterSolution("day14part1", func(logger *log.Logger) string {
 		return part1(logger, 635041, 10)
+	})
+
+	util.RegisterSolution("day14part2", func(logger *log.Logger) string {
+		return part2(logger, 635041)
 	})
 }
--- day14/day14_test.go
+++ day14/day14_test.go
@@ -26,3 +26,25 @@ func TestPart1Impl(t *testing.T) {
 		}
 	}
 }
+
+func TestPart2Impl(t *testing.T) {
+	logger := log.New(os.Stdout, "", 0)
+	tables := []struct{
+		match []byte
+		result int
+	}{
+		{[]byte{5, 1, 5, 8, 9}, 9},
+		{[]byte{0, 1, 2, 4, 5}, 5},
+		{[]byte{9, 2, 5, 1, 0}, 18},
+		{[]byte{5, 9, 4, 1, 4}, 2018},
+	}
+
+	for _, table := range tables {
+		result := part2impl(logger, table.match)
+		if result != table.result {
+			t.Errorf("expected %v, got %v", table.result, result)
+		}
+	}
+}
+
+
